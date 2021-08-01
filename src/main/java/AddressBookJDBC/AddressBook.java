1eed9a802b65fd009726e0984d58158c77f5e330
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-08-01 22:40:54 +0530

    Added: To reterive data from address book for particular city/date

--- src/main/java/AddressBookJDBC/AddressBook.java
+++ src/main/java/AddressBookJDBC/AddressBook.java
@@ -5,50 +5,60 @@ import java.util.List;
 import java.util.Scanner;
 
 public class AddressBook {
-	
-static final Scanner s = new Scanner(System.in);
-	
-public static void main(String[] args) throws ClassNotFoundException, SQLException {
-	
-	System.out.println(" Press 1 to Reterive data\n Press 2 to Update data\n Press 3 to Reterive Data for Particular Date");
-	int choice = s.nextInt();
-	
-	switch(choice) {
-	case 1:
-		  ReteriveData();
-		  break;
-	case 2:
-		  UpdateData();
-		  break;
-	case 3:
-		  ReteriveDataForParticularDate();
-		  break;	  
+
+	static final Scanner s = new Scanner(System.in);
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		System.out.println(
+				" Press 1 to Reterive data\n Press 2 to Update data\n Press 3 to Reterive Data for Particular Date"
+						+ "\n Press 4 to Reterive Data for city or state");
+		int choice = s.nextInt();
+
+		switch (choice) {
+		case 1:
+			ReteriveData();
+			break;
+		case 2:
+			UpdateData();
+			break;
+		case 3:
+			ReteriveDataForParticularDate();
+			break;
+		case 4:
+			ReteriveDataForParticularCityorState();
+			break;
+		}
 	}
-}
-	
+
 	private static void ReteriveData() throws SQLException {
 		AddressBookRepo repo = new AddressBookRepo();
 		List<Contacts> contact = repo.findAll();
 		contact.forEach(System.out::println);
 	}
-		
-	
+
 	private static void UpdateData() throws SQLException {
 		System.out.println("Enter Contact_Id");
-	    int Contact_ID = s.nextInt();
-		
+		int Contact_ID = s.nextInt();
+
 		System.out.println("Enter Address");
-	    String Address = s.next();
-		
-	    AddressBookRepo repo = new AddressBookRepo();
+		String Address = s.next();
+
+		AddressBookRepo repo = new AddressBookRepo();
 		repo.updatedata(Contact_ID, Address);
-			
+
 	}
-	
+
 	private static void ReteriveDataForParticularDate() throws SQLException {
 		AddressBookRepo repo = new AddressBookRepo();
 		List<Contacts> infos = repo.findAllForParticularDate();
 		infos.forEach(System.out::println);
-		
+
+	}
+	
+	private static void ReteriveDataForParticularCityorState() throws SQLException {
+    	AddressBookRepo repo = new AddressBookRepo();
+		List<Contacts> infos = repo.findAllForParticularCityorState();
+		infos.forEach(System.out::println);	
 	}
 }
--- src/main/java/AddressBookJDBC/AddressBookRepo.java
+++ src/main/java/AddressBookJDBC/AddressBookRepo.java
@@ -178,4 +178,67 @@ public class AddressBookRepo {
 		}
 		return infos;
 	}
+	
+public List<Contacts> findAllForParticularCityorState() throws SQLException {
+		
+		List<Contacts> infos=new ArrayList<>();
+		Connection connection = null;
+		PreparedStatement prepstatement = null;
+		try {
+			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver ());
+			
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook_service?allowPublicKeyRetrieval=true&useSSL=false", "root",
+					"pass123");
+			
+			String query ="Select * from AddressBook where City='Bangalore' or State='Uttarakhand'";
+			prepstatement = connection.prepareStatement(query);
+			
+			ResultSet resultset = prepstatement.executeQuery();
+			
+			while(resultset.next()) {
+				Contacts information = new Contacts();
+				
+				int id=resultset.getInt(1);
+				information.setContact_Id(id);
+				
+				String firstname = resultset.getString(2);
+				information.setFirstName(firstname);
+				
+				String lastname = resultset.getString(3);
+				information.setLastName(lastname);
+				
+				String address = resultset.getString(4);
+				information.setAddress(address);
+				
+				String city = resultset.getString(5);
+				information.setCity(city);
+				
+				String state = resultset.getString(6);
+				information.setState(state);
+				
+				String zip = resultset.getString(7);
+				information.setZipCode(zip);
+				
+				String phoneNumber = resultset.getString(8);
+				information.setPhoneNumber(phoneNumber);
+				
+				String emailId = resultset.getString(9);
+				information.setEmailId(emailId);
+				
+				infos.add(information);
+			}
+			}catch (SQLException e) {
+				e.printStackTrace();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}finally {
+				if(connection != null) {
+					connection.close();
+				}
+				if(prepstatement != null) {
+				   prepstatement.close();
+				}
+			}
+			return infos;
+	}
 }
