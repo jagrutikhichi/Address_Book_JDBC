f2fb3e8c04d23380b76995cebd8d8ffda943cf7a
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-08-01 19:43:05 +0530

    Added: To update data in the addressbook table

--- src/main/java/AddressBookJDBC/AddressBook.java
+++ src/main/java/AddressBookJDBC/AddressBook.java
@@ -6,12 +6,40 @@ import java.util.Scanner;
 
 public class AddressBook {
 	
+static final Scanner s = new Scanner(System.in);
+	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		Scanner s = new Scanner(System.in);
 		
+		System.out.println(" Press 1 to Reterive data\n Press 2 to Update data\n");
+		int choice = s.nextInt();
+		
+		switch(choice) {
+		case 1:
+			  ReteriveData();
+			  break;
+		case 2:
+			  UpdateData();
+			  break;
+		}
+	}
+	
+	private static void ReteriveData() throws SQLException {
 		AddressBookRepo repo = new AddressBookRepo();
 		List<Contacts> contact = repo.findAll();
 		contact.forEach(System.out::println);
+	}
+		
+	
+	private static void UpdateData() throws SQLException {
+		System.out.println("Enter Contact_Id");
+	    int Contact_ID = s.nextInt();
+		
+		System.out.println("Enter Address");
+	    String Address = s.next();
+		
+	    AddressBookRepo repo = new AddressBookRepo();
+		repo.updatedata(Contact_ID, Address);
+			
 	}
 	
 }
--- src/main/java/AddressBookJDBC/AddressBookRepo.java
+++ src/main/java/AddressBookJDBC/AddressBookRepo.java
@@ -75,6 +75,40 @@ public class AddressBookRepo {
 			}
 		 }
 		 return infos;
-		
 	}
+	
+	public void updatedata(int contact_ID, String address) throws SQLException {
+		
+	    Connection connection = null;
+		PreparedStatement prepstate = null;
+		try {
+		//Step1: Load & Register Driver Class
+		DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+		
+		//Step2: Establish a MySql Connection
+		 connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/addressbook_service?allowPublicKeyRetrieval=true&useSSL=false", "root", "pass123");
+		
+		//Step3: Create Statement
+		 String query ="Update AddressBook set address=? where contact_ID=?";
+		 prepstate = connection.prepareStatement(query);
+		 prepstate.setString(1, address);
+		 prepstate.setInt(2, contact_ID);
+		
+		//Step4: Execute Query
+		 prepstate.executeUpdate();
+		 System.out.print("Records Updated!");
+		
+		 }catch (SQLException e) {
+			e.printStackTrace();
+		 }catch (Exception e) {
+			e.printStackTrace();
+		 }finally {
+			if(connection != null) {
+			   connection.close();
+			}
+			if(prepstate != null) {
+			   prepstate.close();
+			}
+		}
+ }
 }
